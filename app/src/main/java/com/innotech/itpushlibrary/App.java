5ac3e336a0512b0466125b1704189079350f42d1
admin <huguangwen@bigbaser.com>
2018-09-06 17:29:24 +0800

    更新个推版本

--- app/src/main/java/com/innotech/itpushlibrary/App.java
+++ app/src/main/java/com/innotech/itpushlibrary/App.java
@@ -2,7 +2,9 @@ package com.innotech.itpushlibrary;
 
 import android.app.Application;
 
+import com.inno.innosdk.pb.InnoMain;
 import com.innotech.innotechpush.InnotechPushManager;
+import com.innotech.innotechpush.sdk.KeepApplication;
 import com.innotech.innotechpush.utils.SPIcon;
 
 import java.util.Random;
@@ -11,7 +13,9 @@ import java.util.Random;
  * Created by admin on 2018/4/11.
  */
 
-public class App extends Application {
+public class App extends KeepApplication {
+
+    int s = 0;
 
     @Override
     public void onCreate() {
@@ -19,9 +23,20 @@ public class App extends Application {
         int max = 10000;
         int min = 1000;
         Random random = new Random();
-        int s = random.nextInt(max) % (max - min + 1) + min;
-        InnotechPushManager.getInstance().initPushSDK(this, "" + s);
-        InnotechPushManager.pushIcon = R.mipmap.ic_shortcut_account_box;
-        InnotechPushManager.getInstance().setPushRevicer(new TestPushReciver());
+        s = random.nextInt(max) % (max - min + 1) + min;
+        startInnoSdk("test", "union", "" + s);
+    }
+
+    private void startInnoSdk(String cid, String ch, String member_id) {
+        InnoMain.setValueMap("ch", ch);//这里填入你们的渠道号
+        InnoMain.setValueMap("member_id", member_id);//这里填入你们的用户id
+        InnoMain.startInno(this, cid, new InnoMain.CallBack() {//cid为注册的业务方id
+            @Override
+            public void getOpenid(String openid, int isnew, String remark) {
+                InnotechPushManager.getInstance().initPushSDK(App.this);
+                InnotechPushManager.pushIcon = R.mipmap.ic_launcher;
+                InnotechPushManager.getInstance().setPushRevicer(new TestPushReciver());
+            }
+        });
     }
 }
